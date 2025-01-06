1c888c02e97452695e1f49c51d632ddc68202334
Michael Schmidt <michael.schmidt4@usda.gov>
2025-01-06 12:23:07 -0700

    added bbs data

---
+++ R/bbs_data.R
@@ -0,0 +1,20 @@
+#' Core Trend Data from the Breeding Bird Survey (USGS) 1966-2022
+#'
+#' @format ## `core_trend`
+"core_trend"
+
+
+#' Expanded Trend Data from the Breeding Bird Survey (USGS), 1993-2022
+#'
+#' @format ## `expanded_trend`
+"expanded_trend"
+
+#' Core Index data from the Breeding Bird Survey (USGS) 1966-2022
+#'
+#' @format ## `core_index`
+"core_index"
+
+#' Expanded Index data from the Breeding Bird Survey (USGS) 1993-2022
+#'
+#' @format ## `expanded_index`
+"expanded_index"
