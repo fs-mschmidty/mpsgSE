5c4b3412f8b7d53a6baabc8855a89d83a763528e
Michael Schmidt <michael.schmidt4@usda.gov>
2025-01-08 10:34:08 -0700

    changed input of get_synonyms to vector from df

--- R/get_synonyms.R
+++ R/get_synonyms.R
@@ -1,14 +1,9 @@
 #' This function takes in a dataframe of species with assigned taxon_ids and outputs a list of potential synonyms from the GBIF Backbone taxonomy.  If more than one species has a taxon_id assigned to it it will only returns synonyms for one species. This returns many fields, however only the cananical name is used.
-#' @param x a data frame
-#' @param taxon_id_field GBIF taxon_id provided by get_taxonomies.
+#' @param x a vector of unique taxon_ids from get_taxonomies
 #' @export
 
-get_synonyms <- function(x, taxon_id_field = "taxon_id") {
-  t_ids <- x |>
-    dplyr::distinct(taxon_id_field) |>
-    dplyr::pull(taxon_id_field)
-
-  lapply(t_ids, function(x) rgbif::name_usage(key = x, data = "synonyms")$data) |>
+get_synonyms <- function(x) {
+  lapply(x, function(x) rgbif::name_usage(key = x, data = "synonyms")$data) |>
     dplyr::bind_rows() |>
     dplyr::mutate(taxon_id = acceptedKey)
 }
