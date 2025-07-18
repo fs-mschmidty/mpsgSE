6354317da63b2deb71760dca0ef9c825e9a7311c
Matthew W. Van Scoyoc <matthew.vanscoyoc@usda.gov>
2025-07-17 18:37:39 -0600

    fixed get_bcc_list.R

--- R/get_bcc_list.R
+++ R/get_bcc_list.R
@@ -13,5 +13,6 @@
 #' install.packages(mpsgSE)
 #' bcc_9 <- get_bcc_list(9)
 get_bcc_list <- function(bcc_region){
+  mpsgSE::bcc_list
   bcc_list[grepl(paste0("\\b", bcc_region, "\\b"), bcc_list$bc_rs_for_bcc_listing_breeding), ]
   }
