7a44bfc537e4128f34bc3efe4b778d4bf3af4c73
Matthew W. Van Scoyoc <matthew.vanscoyoc@usda.gov>
2025-07-24 16:57:06 -0600

    added fs_units data

---
+++ R/fs_units.R
@@ -0,0 +1,6 @@
+#' List of Forest Service Units
+#' 
+#' `fs_units` List of USDA Forest Service units from the Forest Service Rest 
+#'     Services, https://apps.fs.usda.gov/arcx/rest/services/EDW.
+
+"fs_units"
---
+++ data-raw/_fs_units.R
@@ -0,0 +1,49 @@
+#' ---
+#' title: "List of Forest Service Units"
+#' author:
+#'   - name: "Matthew Van Scoyoc" 
+#'     affiliation: |
+#'       | Mountain Planning Service Group, Regions 1-4
+#'       | Information Management Group
+#'       | Forest Service, USDA
+#' date: 24 July, 2025
+#' 
+#' This script queries taxonomy and taxon IDs using the `get_taxonomies()` 
+#' function.
+#'-----------------------------------------------------------------------------
+
+
+# setup ----
+pkgs <- c("dplyr",   # data management
+          "janitor", # tidy up data frames
+          "mpsgSE",  # taxonomy function
+          "readr",   # read/write data
+          "readxl",  # read Excel files
+          "tidyr",   # data management
+          "usethis") # package development
+
+# Install packages if they aren't in your library
+inst_pkgs <- pkgs %in% rownames(installed.packages())
+if (any(inst_pkgs == FALSE)) {
+  install.packages(pkgs[!inst_pkgs], 
+                   lib =  .libPaths()[1], 
+                   repos = "https://cloud.r-project.org",
+                   type = 'source', 
+                   dependencies = TRUE, 
+                   quiet = TRUE)
+}
+
+# Load packages
+invisible(lapply(pkgs, library, character.only = TRUE))
+
+
+# data ----
+fs_units <- read_edw_lyr("EDW_ForestSystemBoundaries_01", layer = 1) |> 
+  sf::st_drop_geometry() |> 
+  dplyr::select(adminforestid, region, forestnumber, forestorgcode, forestname, 
+                gis_acres)
+
+# save ----
+readr::write_csv(fs_units,
+                 file.path("data-raw/output/species_lists/fs_units.csv"))
+usethis::use_data(fs_units, overwrite = TRUE)
