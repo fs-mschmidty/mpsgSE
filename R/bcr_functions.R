903cf55f9f3263466783e31b065c51e1230562c0
Matthew W. Van Scoyoc <matthew.vanscoyoc@usda.gov>
2025-09-29 16:08:02 -0600

    added build_bbs_trend_narratives.R

--- R/bcr_functions.R
+++ R/bcr_functions.R
@@ -4,20 +4,27 @@
 #' This function returns a data frame of bird species that occur in the Birds of 
 #'     Conservation Regions a National Forest or Grassland overlaps.
 #'
-#' @param sf_lyr An `sf` object of the Administrative Boundary of a Natioal 
+#' @param sf_lyr An `sf` object of the Administrative Boundary of a National 
 #'                   Forest or Grassland.
 #'
-#' @returns A data frame.
+#' @returns A [tibble::tibble()].
+#' 
 #' @export
 #'
 #' @examples
+#' ## Not run:
+#' 
+#' library("mpsgSE")
+#' 
 #' # Pull the Administrative Boundary for the Dixie National Forest
 #' adm_bdy = mpsgSE::read_edw_lyr("EDW_ForestSystemBoundaries_01", layer = 1) |>
 #'   dplyr::filter(region == "04" & forestnumber == "07")
 #' 
 #' # Get list of birds of conservation concern
-#' mpsgSE::get_bcc_list(adm_bdy)
-get_bcc_list <- function(sf_lyr){
+#' mpsgSE::get_bcr_list(adm_bdy)
+#'  
+#' ## End(Not run)                     
+get_bcr_list <- function(sf_lyr){
   bcrs = mpsgSE::get_bcc_regions(sf_lyr) |> dplyr::pull(bcr_label)
   bcc = mpsgSE::bcc_list
   
@@ -25,26 +32,37 @@ get_bcc_list <- function(sf_lyr){
     bcc[grepl(paste0("\\b", bcr_code, "\\b"), bcc$bc_rs_for_bcc_listing_breeding), ]
     }
   
-  bcr_list = lapply(bcrs, get_list) |> dplyr::bind_rows() |> dplyr::distinct()
+  bcr_list = lapply(bcrs, get_list) |> 
+    dplyr::bind_rows() |> 
+    dplyr::distinct() |> 
+    tibble::tibble()
   return(bcr_list)
   }
 
 
 #' Clip Birds of Conservation Regions to a polygon
 #' 
-#' This function clips the Birds of Conservaiton Regions [sf] object to a 
-#'     polygon
+#' This function clips the Birds of Conservation Regions [sf] object to a 
+#'     polygon.
 #'
-#' @param sf_lyr [sf] object.
+#' @param sf_lyr [sf] polygon object.
 #'
 #' @returns A [tibble::tibble()].
+#' 
 #' @export
 #'
 #' @examples
+#' ## Not run:
+#' 
+#' library("mpsgSE")
+#' 
+#' # Pull the Administrative Boundary for the Dixie National Forest
 #' adm_bdy <- mpsgSE::read_edw_lyr("EDW_ForestSystemBoundaries_01", layer = 1) |> 
 #'   dplyr::filter(region == "04" & forestnumber == "07")
-#' get_bcc_regions(adm_bdy)
-get_bcc_regions <- function(sf_lyr){
+#' get_bc_regions(adm_bdy)
+#'  
+#' ## End(Not run)                     
+get_bc_regions <- function(sf_lyr){
   sf::st_intersection(
     sf::st_make_valid(sf_lyr),
     sf::st_transform(mpsgSE::bcc_regions, crs = sf::st_crs(sf_lyr))
@@ -52,5 +70,6 @@ get_bcc_regions <- function(sf_lyr){
     sf::st_drop_geometry() |> 
     dplyr::select(-dplyr::contains("shape") & -dplyr::contains("Shape") & 
                     -dplyr::contains("SHAPE") & -dplyr::contains("gis")) |> 
+    tibble::tibble() |> 
     suppressWarnings()
 }
---
+++ R/build_bbs_trend_narratives.R
@@ -0,0 +1,62 @@
+#' Build Narratives for Breeding Bird Survey Trend Data
+#' 
+#' This function builds the USGS Breeding Bird Survey (BBS) trend narratives for 
+#'     bird species. The USGS breeding bird survey data can be found here:
+#'     https://www.sciencebase.gov/catalog/item/67aba702d34e329fb20457b1
+#'
+#' @param region_codes Vector of BBS region codes.
+#' @param region_names Vector of BBS region names.
+#' 
+#' @returns A [tibble::tibble()].
+#' 
+#' @export
+#' 
+#' @examples
+#' ## Not run:
+#' 
+#' bbs_trends <-  build_bbs_trend_narratives(
+#'   region_codes = c("IDA", "MON", "S10", "US1"),
+#'   region_names = c("Idaho", "Montana",
+#'                    "Northern Rockies Bird Conservation Region (S10)",
+#'                    "the United States")
+#'   )
+#'  
+#' ## End(Not run)                     
+build_bbs_trend_narratives <- function(region_codes, region_names){
+  region_df = tibble::tibble(region = region_codes,
+                             region_long_name = region_names)
+  
+  bbs_narratives = mpsgSE::core_trend |>
+    dplyr::filter(region %in% region_df$region) |>
+    dplyr::left_join(region_df, by = "region") |>
+    dplyr::mutate(
+      trend_description = dplyr::case_when(significance == 1 ~ "increasing",
+                                           significance == 2 ~ "decreasing",
+                                           is.na(significance) ~ "uncertain"),
+      narrative_chunk = glue::glue(
+        "in {region_long_name} the trend is {trend_description} with a trend of {trend}% [95% CI {x2_5_percent_ci}%, {x97_5_percent_ci}%]"
+      )
+    ) |> 
+    dplyr::group_by(species) |>
+    dplyr::summarize(
+      narrative_total_chunk = paste(narrative_chunk, collapse = "; "),
+      n = dplyr::n()
+    ) |>
+    dplyr::mutate(
+      citation = "(Hostelter et al. 2025 via USDA Forest Service 2025)",
+      narrative_total_chunk = ifelse(
+        n == 2, 
+        stringr::str_replace(narrative_total_chunk, ";", " and"),
+        stringr::str_replace(narrative_total_chunk, ";(?=[^;]*$)", ", and")
+      ),
+      narrative_total_chunk = stringr::str_replace(narrative_total_chunk, 
+                                                   "i", "I"),
+      narrative_total_chunk = stringr::str_replace_all(narrative_total_chunk, 
+                                                       ";", ","),
+      final_narrative = glue::glue(
+        "The USGS estimates population trend with data from the North American Breeding Bird Survey collected from 1966 to 2022. {narrative_total_chunk} {citation}."
+      )
+    )
+  
+  return(bbs_narratives)
+}
