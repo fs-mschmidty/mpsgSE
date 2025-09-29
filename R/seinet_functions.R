f34638c854692fe933dd09288d9107ffb9300cd8
Matthew W. Van Scoyoc <matthew.vanscoyoc@usda.gov>
2025-09-29 13:14:44 -0600

    optimized seinet functions

--- R/seinet_functions.R
+++ R/seinet_functions.R
@@ -52,7 +52,7 @@ build_seinet_spatial_data <- function(sei_data, spp_list) {
   # Filter spatial data
   elig_sei = sei_data |> 
     dplyr::select(dplyr::any_of(var_names)) |> 
-    dplyr::filter(SEINet_taxonID %in% spp_list$taxon_id)
+    dplyr::filter(taxon_id %in% spp_list$taxon_id)
   
   return(elig_sei)
 }
@@ -60,7 +60,9 @@ build_seinet_spatial_data <- function(sei_data, spp_list) {
 
 #' Read SEINet data into R
 #' 
-#' This function reads SEINet data in Darwin Core Archive format into R.
+#' This function reads SEINet data in Darwin Core Archive format into R and 
+#'     pulls taxon ID's and taxonomies from GBIF using `get_taxonomies()` 
+#'     function.
 #'
 #' @param dir_path Path to SEINet data folder.
 #' @param crs Target coordinate reference system (CRS). Either and 
@@ -69,6 +71,8 @@ build_seinet_spatial_data <- function(sei_data, spp_list) {
 #'                resulting sf object CRS will be WGS84.
 #'
 #' @return An `sf` class object.
+#' 
+#' @seealso [get_taxonomies()]
 #' 
 #' @details
 #' Additional details...
@@ -95,7 +99,7 @@ get_seinet_data <- function(dir_path, crs = NULL){
                    "ymd", "ymd HM")
   
   #  Read data into R
-  dat = readr::read_csv(data_path, show_col_types = FALSE) |> 
+  raw_dat = readr::read_csv(data_path, show_col_types = FALSE) |> 
     dplyr::filter(taxonRank %in% c('Species', 'Variety', 'Subspecies')) |> 
     dplyr::filter(!is.na(decimalLatitude) | !is.na(decimalLongitude)) |> 
     dplyr::mutate(
@@ -107,31 +111,40 @@ get_seinet_data <- function(dir_path, crs = NULL){
       ) |> 
     dplyr::rename("SEINet_taxonID" = taxonID) |> 
     sf::st_as_sf(coords = c("decimalLongitude", "decimalLatitude"), 
-                 crs = "WGS84")
+                 crs = "EPSG:4326")
   # Get taxonomies
-  taxonomy = sf::st_drop_geometry(dat) |> 
+  gbif_tids = sf::st_drop_geometry(raw_dat) |> 
     dplyr::select(SEINet_taxonID, scientificName) |>
     dplyr::distinct() |> 
-    mpsgSE::get_taxonomies() |> 
-    dplyr::select(SEINet_taxonID, taxon_id)
+    mpsgSE::get_taxonomies(query_field = "scientificName") |> 
+    dplyr::select(-scientificName)
   
-  dat = dplyr::left_join(dat, taxonomy, 
-                         by = "SEINet_taxonID", relationship = 'many-to-many')
+  # Rename SEINet taxonomy variables that are duplicated in GBIF
+  gbif_vars = colnames(gbif_tids)[!colnames(gbif_tids) %in% 
+                                    c("scientificName", "SEINet_taxonID")]
+  sei_vars = colnames(raw_dat)[colnames(raw_dat) %in% gbif_vars]
+  new_vars = paste0("SEINet_", sei_vars)
+  colnames(raw_dat)[colnames(raw_dat) %in% sei_vars] = new_vars
+  
+  sei_data = dplyr::left_join(raw_dat, gbif_tids, 
+                              by = "SEINet_taxonID", 
+                              relationship = 'many-to-many')
   # Re-project CRS
   if(!is.null(crs)){
-    if(sf::st_crs(dat) != crs) dat = sf::st_transform(dat, crs = crs)
+    if(sf::st_crs(sei_data) != crs){
+      sei_data = sf::st_transform(sei_data, crs = crs)
+      }
   }
   
-  return(dat)
+  return(sei_data)
 }
 
 #' Summarize SEINet data by species
 #' 
-#' This function summarizes the spatial SEINet object from `get_seinet_data()` by 
-#'     species. The summary includes the number of records per species, minimum 
-#'     and maximum year a species is observed, and the SEINet occurrence ID if 
-#'     there are less than seven (7) observations. This function then verifies 
-#'     taxonomy using the `get_taxonomies()` function.
+#' This function summarizes the spatial SEINet object from `get_seinet_data()` 
+#'     by species. The summary includes the number of records per species, 
+#'     minimum and maximum year a species is observed, and the SEINet occurrence ID if 
+#'     there are less than seven (7) observations.
 #'
 #' @param seinet_data Spatial SEINet data from `get_seinet_data()`.
 #'
@@ -160,36 +173,51 @@ build_seinet_spp <- function(seinet_data, locale = TRUE){
   # Date formats
   date_formats = c("%Y-%m-%d %H:%M:%S", "%Y-%m-%d", "%Y-%m", "%Y", "ymd HMS", 
                    "ymd", "ymd HM")
+  taxa_select = c("taxon_id", "kingdom", "phylum", "class", "order", "family", 
+                  "genus", "species", "subspecies", "variety", "form")
+  
   # Locale
   if(isTRUE(locale)){
     locale = stringr::str_c(unique(seinet_data$locale), collapse = ", ")
   }
   
+  # Subset species names
+  t_ids = sf::st_drop_geometry(seinet_data) |> 
+    dplyr::select(taxon_id, SEINet_taxonID, scientificName) |> 
+    dplyr::filter(!taxon_id == "" | !is.na(taxon_id)) |> 
+    dplyr::distinct() |> 
+    dplyr::mutate(dup_taxon = ifelse(duplicated(taxon_id) | 
+                                       duplicated(taxon_id, fromLast = TRUE),
+                                     "Yes", "No"))
   # Summarize data
-  dat = sf::st_drop_geometry(seinet_data) |> 
-    dplyr::select(taxonID, occurrenceID, scientificName, eventDate) |> 
+  t_id_sum = sf::st_drop_geometry(seinet_data) |> 
+    dplyr::select(taxon_id, occurrenceID, eventDate) |> 
     dplyr::mutate(
       date = lubridate::parse_date_time(eventDate, date_formats) |> as.Date(),
       year = lubridate::year(date)
     ) |> 
-    dplyr::rename("scientific_name" = scientificName) |>
-    dplyr::distinct() |> 
-    dplyr::group_by(scientific_name) |> 
+    dplyr::group_by(taxon_id) |> 
     dplyr::summarize(
       nObs = dplyr::n(),
-      minYear = min(lubridate::year(date), na.rm = TRUE),
-      maxYear = max(lubridate::year(date), na.rm = TRUE),
-      SEINet_taxonID = stringr::str_c(unique(taxonID), collapse = ", "),
+      minYear = min(year, na.rm = TRUE),
+      maxYear = max(year, na.rm = TRUE),
       occID = ifelse(nObs <= 6, 
                      stringr::str_c(unique(occurrenceID), collapse = ", "),
                      NA),
       .groups = "drop"
       ) |> 
-    dplyr::mutate(locale = locale, source = "SEINet") |> 
-    dplyr::filter(!scientific_name == "") |> 
-    dplyr::distinct(scientific_name, .keep_all = TRUE) |> 
-    mpsgSE::get_taxonomies(query_field = "scientific_name") |> 
+    dplyr::mutate(locale = locale, source = "SEINet")
+  # Subset taxonomy
+  taxa = sf::st_drop_geometry(seinet_data) |> 
+    dplyr::select(dplyr::any_of(taxa_select)) |> 
+    dplyr::distinct()
+  
+  
+  dat = dplyr::left_join(t_ids, t_id_sum, by = "taxon_id", 
+                         relationship = 'many-to-many')|> 
+    dplyr::left_join(taxa, by = 'taxon_id', relationship = 'many-to-many') |> 
     dplyr::arrange(kingdom, phylum, class, order, family, genus, 
-                   species, scientific_name)
+                   species, scientificName) |> 
+    dplyr::rename("scientific_name" = scientificName)
   return(dat)
 }
